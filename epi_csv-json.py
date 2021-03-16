b4ccf14ad50bf1d9970347e216148d0358112a7d
rajeevdas2507 <rajeevdas25@gmail.com>
2021-03-16 12:19:33 -0400

    Added the location array sectione (This will be modified further in future)

--- epi_csv-json.py
+++ epi_csv-json.py
@@ -28,17 +28,16 @@ import csv as cv
 ## Define the paths (The paths here are those that considered the CITADEL infrastructure)
 
 
-#pathOfEpifile = '/data8/projets/Mila_covid19/output/covidb_full/csv/episode_data.csv'
-#pathofEpiJsonfile = '/data8/network_mount/S/FHIR_json/episode_data.json'
-
-
 
 #pathOfEpifile = '/data8/projets/Mila_covid19/output/covidb_anon/csv/episode_data.csv'
-#pathofEpiJsonfile = '/data8/network_mount/S/FHIR_json/Final_Sep_21/episode_data.json'
+
 
 
 pathOfEpifile = '/data8/network_mount/S/CODA19_Anon_csv/episode_data.csv'
-pathofEpiJsonfile = '/data8/network_mount/S/FHIR_json/Mapped_Files_Nov_17/episode_data.json'
+pathofEpiJsonfile = '/data8/network_mount/S/FHIR_json/Mapped_Files_Nov_17/episode_data_20.json'
+
+
+path_to_dictionary = '/data8/projets/ChasseM_CODA19_1014582/fhir/code/rdas/files_mapping/chum.json'
 
 
 ## Load and process (if required) the data using Pandas and the csv file.
@@ -48,10 +47,42 @@ dfEpi = pd.read_csv(pathOfEpifile)
 
 
 
+def read_dictionary(path_to_dictionary_file):
+    
+    
+    """
+    
+    Considers path of the json file. 
+    
+    
+    Arguments:
+        
+        path_to_setting_file: A string
+        
+    Returns: 
+        
+        data: Object 
+    
+    
+    """
+    
+   
+    try:
+        with open(path_to_dictionary_file) as data_file:
+            data = js.load(data_file)
+            data_file.close()
+            return data
+    
+    except IOError as e:
+        
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+        return -1
+
+
 
     
     
-def epi_dic_json(dfepisode):
+def epi_dic_json(dfepisode,dic_chum):
     
     
     """
@@ -68,7 +99,62 @@ def epi_dic_json(dfepisode):
     
     dict_json = {}
     
-    for i in range(len(dfepisode)):
+    for i in range(20):
+        
+        
+        
+        
+        ## Fetch the array/list name.
+        
+        key_exist = dic_chum.get("unitType","None")
+        
+        
+        if(key_exist == 'None'):
+            
+            
+            system_input = 'http://terminology.hl7.org/ValueSet/v3-ServiceDeliveryLocationRoleType'            
+            code_input = ''                        
+            display_complete_input = 'no code available'
+            
+            
+            
+        else:
+        
+          
+          ## consider the dictionary/object in the json dictionary using the key and fetch
+          ## the values. Here the first three characters are considered to perform a match
+          ## check. In the present scenario this was a simpler approach.   
+            
+            
+          for k in range(len(dic_chum["unitType"])):   
+           
+              
+             
+               string_check = dic_chum["unitType"][k]['display_string'][:5]
+               
+               
+              
+               if(string_check == dfepisode.iloc[i]["episode_unit_type"][:5]):   
+                              
+                   system_input = dic_chum["unitType"][k]['fhir_reference_url']          
+                   code_input = dic_chum["unitType"][k]['fhir_code']                        
+                   display_complete_input = dic_chum["unitType"][k]['display_string']
+                   
+                   
+                   
+                   break
+                   
+               else:
+                   
+                   system_input = 'http://terminology.hl7.org/ValueSet/v3-ServiceDeliveryLocationRoleType'            
+                   code_input = ''                        
+                   display_complete_input = 'no code available'                
+                   
+        
+         
+        
+        
+        
         
         single_json = { 
                 
@@ -78,13 +164,30 @@ def epi_dic_json(dfepisode):
                            "id": dfepisode.iloc[i]["episode_admission_uid"],    ## dfepisode.iloc[i]["patient_site_uid"]                                   
                                                               
                                                                                   
-                           "class": {"system": "http://terminology.hi17.org/CodeSystem/v3-ActCode",
+                           "class": {"system": "http://terminology.hi17.org/CodeSystem/v3-ActCode",                     
                                                                             
-                                      "code": "IMP",
-                                      "display": dfepisode.iloc[i]["episode_description"]},
+                                      "code": 'IMP',                         
+                                      "display": dfepisode.iloc[i]["episode_description"]},        
                                       
                            
                            "subject":{"reference": str(dfepisode.iloc[i]["patient_site_uid"])},
+                           
+                           "location": [ {                                    
+                                         "location":{ "reference": code_input,
+                                         "display":  display_complete_input
+                                       },
+                                   
+                                       "status": "completed",
+                                       
+                                       "period": {
+                                                  "start": dfepisode.iloc[i]["episode_start_time"],
+                                                  "end": dfepisode.iloc[i]["episode_end_time"]
+                                                  }
+                                       
+                                       }
+                                   
+                                   ],
+                           
                            
                            
                            "reasonCode": [
@@ -106,11 +209,23 @@ def epi_dic_json(dfepisode):
     return(dict_json)
  
 
+
+
+
+## load thw dictionary
+  
+    
+dict_data = read_dictionary(path_to_dictionary) 
+
+
+
+
+
 ## Call the function to create the required json structure using
 ## dictionary.
 
 
-dictJsonEpi = epi_dic_json(dfEpi)
+dictJsonEpi = epi_dic_json(dfEpi,dict_data)
 
 ## dictJsonEpi["17808"] #check
        
